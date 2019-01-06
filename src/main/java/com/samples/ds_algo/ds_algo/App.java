7a69f947c1cca969a1b7165d006a0cff680bc520
deeptisingh113 <deepti.singh113@gmail.com>
2019-01-06 19:25:16 +0530

    # Added first draft of the project

---
+++ src/main/java/com/samples/ds_algo/ds_algo/App.java
@@ -0,0 +1,13 @@
+package com.samples.ds_algo.ds_algo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
